70a2864d765d1c236d710be2a1f4a088155de521
liuzhang <liuzhang@liuzhangdeMacBook-Pro.local>
2018-05-22 14:36:40 +0800

    资源包练习

---
+++ src/MyTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liuzhang
+ * Date: 2018/4/5
+ * Time: 上午7:02
+ */
+
+namespace Liuzhang;
+
+
+class MyTest
+{
+    function say()
+    {
+        echo 'ok';
+    }
+}
---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liuzhang
+ * Date: 2018/4/5
+ * Time: 上午7:01
+ */
+require_once 'vendor/autoload.php';
+use Liuzhang\MyTest;
+
+$test = new MyTest();
+$test->say();
